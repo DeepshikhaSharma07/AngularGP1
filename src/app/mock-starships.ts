46547b00f58c11feb5dfc866ce777c1e179c8a06
DeepshikhaSharma07 <79154976+DeepshikhaSharma07@users.noreply.github.com>
2021-04-02 16:47:06 -0400

    group project -final project
    made by Deeps

---
+++ src/app/mock-starships.ts
@@ -0,0 +1,14 @@
+import { Starship } from './starship';
+
+export const STARSHIPS: Starship[] = [
+  { id: 1, name: 'Imperial-I Class Destroyer', commander: 'Galactic Empire'},
+  { id: 2, name: 'Assertor-Class Dreadnought', commander: 'Galactic Empire'},
+  { id: 3, name: 'Scarran Dreadnought', commander: 'Scarran Hierarchy'},
+  { id: 4, name: 'Basestar', commander: 'Cylon Empire'},
+  { id: 5, name: 'Dalek Command Saucer', commander: 'Dalek Empire'},
+  { id: 6, name: 'Heighliner Type Jumpship', commander: 'Spacing Guild'},
+  { id: 7, name: 'Leviathan', commander: 'Zerg'},
+  { id: 8, name: 'Reverence-Class Cruiser', commander: 'Covenant Empire'},
+  { id: 9, name: 'Athena Class Command Ship', commander: 'Terran Federation'},
+  { id: 10, name: 'Krooze-Class Cruiser', commander: 'Orks'}
+];
